e1d04cca2cba18e5a74fad02089b37a4985d3cff
Laurrennicole <82077118+Laurrennicole@users.noreply.github.com>
2021-05-21 14:11:35 -0700

    Update scripts.js
    added function code to my menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello Friend! Welcome to my portfolio!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+  }
+}
